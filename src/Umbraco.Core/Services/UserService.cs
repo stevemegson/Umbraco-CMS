stevemegson/Umbraco-CMS

commit d3b69e04e1e5f5620e18c805d5cf0b265f140f96

Updated IUserService and added more unit tests for new methods. Fixed unit tests that were failing due to incorrectly set settings... not sure how this is working on the server but oh well.

src/Umbraco.Core/Services/UserService.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Globalization;
 using System.Linq;
+using Umbraco.Core.Events;
+using Umbraco.Core.Models;
 using Umbraco.Core.Models.Membership;
 using Umbraco.Core.Models.Rdbms;
 using Umbraco.Core.Persistence;
@@ -102,6 +104,45 @@ namespace Umbraco.Core.Services
         }
 
         /// <summary>
+        /// Savers changes to a user to the database
+        /// </summary>
+        /// <param name="user"></param>
+        public void SaveUser(IUser user)
+        {
+            if (UserSaving.IsRaisedEventCancelled(new SaveEventArgs<IUser>(user), this))
+                return;
+
+            var uow = _uowProvider.GetUnitOfWork();
+            using (var repository = _repositoryFactory.CreateUserRepository(uow))
+            {
+                repository.AddOrUpdate(user);
+                uow.Commit();
+            }
+
+            UserSaved.RaiseEvent(new SaveEventArgs<IUser>(user, false), this);
+        }
+
+        /// <summary>
+        /// This is useful for when a section is removed from config
+        /// </summary>
+        /// <param name="sectionAlias"></param>
+        public void DeleteSectionFromAllUsers(string sectionAlias)
+        {
+            var uow = _uowProvider.GetUnitOfWork();
+            using (var repository = _repositoryFactory.CreateUserRepository(uow))
+            {
+                var assignedUsers = repository.GetUsersAssignedToSection(sectionAlias);
+                foreach (var user in assignedUsers)
+                {
+                    //now remove the section for each user and commit
+                    user.RemoveAllowedSection(sectionAlias);
+                    repository.AddOrUpdate(user);
+                }
+                uow.Commit();
+            }
+        }
+
+        /// <summary>
         /// Creates a new user for logging into the umbraco backoffice
         /// </summary>
         /// <param name="name"></param>
@@ -110,7 +151,7 @@ namespace Umbraco.Core.Services
         /// <param name="userType"></param>
         /// <param name="email"></param>
         /// <returns></returns>
-        public IMembershipUser CreateUser(string name, string login, string password, IUserType userType, string email = "")
+        public IMembershipUser CreateMembershipUser(string name, string login, string password, IUserType userType, string email = "")
         {
             var uow = _uowProvider.GetUnitOfWork();
             using (var repository = _repositoryFactory.CreateUserRepository(uow))
@@ -142,5 +183,15 @@ namespace Umbraco.Core.Services
         }
 
         #endregion
+
+        /// <summary>
+        /// Occurs before Save
+        /// </summary>
+        public static event TypedEventHandler<IUserService, SaveEventArgs<IUser>> UserSaving;
+
+        /// <summary>
+        /// Occurs after Save
+        /// </summary>
+        public static event TypedEventHandler<IUserService, SaveEventArgs<IUser>> UserSaved;
     }
 }

src/Umbraco.Core/Persistence/Repositories/Interfaces/IUserRepository.cs
@@ -1,4 +1,5 @@
-﻿using Umbraco.Core.Models.Membership;
+﻿using System.Collections.Generic;
+using Umbraco.Core.Models.Membership;
 
 namespace Umbraco.Core.Persistence.Repositories
 {
@@ -12,6 +13,6 @@ namespace Umbraco.Core.Persistence.Repositories
         /// This is useful when an entire section is removed from config
         /// </summary>
         /// <param name="sectionAlias"></param>
-        void DeleteSectionFromAllUsers(string sectionAlias);
+        IEnumerable<IUser> GetUsersAssignedToSection(string sectionAlias);
     }
 }

src/Umbraco.Core/Persistence/Repositories/UserRepository.cs
@@ -60,18 +60,8 @@ namespace Umbraco.Core.Persistence.Repositories
             }
 
             var sql = GetBaseQuery(false);
-            var foundUserTypes = new Dictionary<short, IUserType>();
-            return Database.Fetch<UserDto, User2AppDto, UserDto>(new UserSectionRelator().Map, sql)
-                           .Select(dto =>
-                           {
-                               //first we need to get the user type
-                               var userType = foundUserTypes.ContainsKey(dto.Type)
-                                                  ? foundUserTypes[dto.Type]
-                                                  : _userTypeRepository.Get(dto.Type);
 
-                               var userFactory = new UserFactory(userType);
-                               return userFactory.BuildEntity(dto);
-                           });
+            return ConvertFromDtos(Database.Fetch<UserDto, User2AppDto, UserDto>(new UserSectionRelator().Map, sql));
         }
 
         private IEnumerable<IUser> PerformGetAllOnIds(params int[] ids)
@@ -110,13 +100,21 @@ namespace Umbraco.Core.Persistence.Repositories
             }
             else
             {
-                sql.Select("*")
-                   .From<UserDto>()
-                   .LeftJoin<User2AppDto>()
-                   .On<UserDto, User2AppDto>(left => left.Id, right => right.UserId);
+                return GetBaseQuery("*");
             }
             return sql;
         }
+
+        private static Sql GetBaseQuery(string columns)
+        {
+            var sql = new Sql();
+            sql.Select(columns)
+                      .From<UserDto>()
+                      .LeftJoin<User2AppDto>()
+                      .On<UserDto, User2AppDto>(left => left.Id, right => right.UserId);
+            return sql;
+        }
+
 
         protected override string GetBaseWhereClause()
         {
@@ -230,12 +228,53 @@ namespace Umbraco.Core.Persistence.Repositories
             var query = Query<IUser>.Builder.Where(x => x.Username == username);
             return GetByQuery(query).FirstOrDefault();
         }
-        
-        public void DeleteSectionFromAllUsers(string sectionAlias)
+
+        public IEnumerable<IUser> GetUsersAssignedToSection(string sectionAlias)
         {
-            throw new NotImplementedException();
+            //Here we're building up a query that looks like this, a sub query is required because the resulting structure
+            // needs to still contain all of the section rows per user.
+
+            //SELECT *
+            //FROM [umbracoUser]
+            //LEFT JOIN [umbracoUser2app]
+            //ON [umbracoUser].[id] = [umbracoUser2app].[user]
+            //WHERE umbracoUser.id IN (SELECT umbracoUser.id
+            //    FROM [umbracoUser]
+            //    LEFT JOIN [umbracoUser2app]
+            //    ON [umbracoUser].[id] = [umbracoUser2app].[user]
+            //    WHERE umbracoUser2app.app = 'content')
+
+            var sql = GetBaseQuery(false);
+            var innerSql = GetBaseQuery("umbracoUser.id");
+            innerSql.Where("umbracoUser2app.app = " + SqlSyntaxContext.SqlSyntaxProvider.GetQuotedValue(sectionAlias));
+            sql.Where(string.Format("umbracoUser.id IN ({0})", innerSql.SQL));
+
+            return ConvertFromDtos(Database.Fetch<UserDto, User2AppDto, UserDto>(new UserSectionRelator().Map, sql));
         }
 
         #endregion
+
+        private IEnumerable<IUser> ConvertFromDtos(IEnumerable<UserDto> dtos)
+        {
+            var foundUserTypes = new Dictionary<short, IUserType>();
+            return dtos.Select(dto =>
+                {
+                    //first we need to get the user type
+                    IUserType userType;
+                    if (foundUserTypes.ContainsKey(dto.Type))
+                    {
+                        userType = foundUserTypes[dto.Type];
+                    }
+                    else
+                    {
+                        userType = _userTypeRepository.Get(dto.Type);
+                        //put it in the local cache
+                        foundUserTypes.Add(dto.Type, userType);
+                    }
+
+                    var userFactory = new UserFactory(userType);
+                    return userFactory.BuildEntity(dto);
+                });
+        } 
     }
 }

src/Umbraco.Core/Services/IMembershipUserService.cs
@@ -1,0 +1,15 @@
+using Umbraco.Core.Models.Membership;
+
+namespace Umbraco.Core.Services
+{
+    /// <summary>
+    /// Defines part of the UserService, which is specific to methods used by the membership provider.
+    /// </summary>
+    /// <remarks>
+    /// Idea is to have this is an isolated interface so that it can be easily 'replaced' in the membership provider impl.
+    /// </remarks>
+    internal interface IMembershipUserService : IService
+    {
+        IMembershipUser CreateMembershipUser(string name, string login, string password, IUserType userType, string email = "");
+    }
+}

src/Umbraco.Core/Services/IUserService.cs
@@ -32,16 +32,17 @@ namespace Umbraco.Core.Services
         /// <param name="name">Name of the UserType to retrieve</param>
         /// <returns><see cref="IUserType"/></returns>
         IUserType GetUserTypeByName(string name);
-    }
 
-    /// <summary>
-    /// Defines part of the UserService, which is specific to methods used by the membership provider.
-    /// </summary>
-    /// <remarks>
-    /// Idea is to have this is an isolated interface so that it can be easily 'replaced' in the membership provider impl.
-    /// </remarks>
-    internal interface IMembershipUserService : IService
-    {
-        IMembershipUser CreateUser(string name, string login, string password, IUserType userType, string email = "");
+        /// <summary>
+        /// Saves changes to the user object
+        /// </summary>
+        /// <param name="user"></param>
+        void SaveUser(IUser user);
+
+        /// <summary>
+        /// This is useful when an entire section is removed from config
+        /// </summary>
+        /// <param name="sectionAlias"></param>
+        void DeleteSectionFromAllUsers(string sectionAlias);
     }
 }

src/Umbraco.Core/Umbraco.Core.csproj
@@ -681,6 +681,7 @@
     <Compile Include="Services\ILocalizationService.cs" />
     <Compile Include="Services\IMediaService.cs" />
     <Compile Include="Services\IMemberService.cs" />
+    <Compile Include="Services\IMembershipUserService.cs" />
     <Compile Include="Services\IService.cs" />
     <Compile Include="Services\IUserService.cs" />
     <Compile Include="Services\LocalizationService.cs" />

src/Umbraco.Tests/Persistence/Repositories/UserRepositoryTest.cs
@@ -439,6 +439,32 @@ namespace Umbraco.Tests.Persistence.Repositories
             AssertPropertyValues(userByUsername, user);
         }
 
+        [Test]
+        public void Get_Users_Assigned_To_Section()
+        {
+            // Arrange
+            var provider = new PetaPocoUnitOfWorkProvider();
+            var unitOfWork = provider.GetUnitOfWork();
+            var repository = RepositoryResolver.Current.ResolveByType<IUserRepository>(unitOfWork);
+            var user1 = MockedUser.CreateUser(CreateAndCommitUserType(), "1", "test", "media");
+            var user2 = MockedUser.CreateUser(CreateAndCommitUserType(), "2", "media", "settings");
+            var user3 = MockedUser.CreateUser(CreateAndCommitUserType(), "3", "test", "settings");
+            repository.AddOrUpdate(user1);
+            repository.AddOrUpdate(user2);
+            repository.AddOrUpdate(user3);
+            unitOfWork.Commit();
+
+            // Act
+
+            var users = repository.GetUsersAssignedToSection("test");
+
+            // Assert            
+            Assert.AreEqual(2, users.Count());
+            var names = users.Select(x => x.Username).ToArray();
+            Assert.IsTrue(names.Contains("TestUser1"));
+            Assert.IsTrue(names.Contains("TestUser3"));
+        }
+
         private void AssertPropertyValues(IUser updatedItem, IUser originalUser)
         {
             Assert.That(updatedItem.Id, Is.EqualTo(originalUser.Id));

src/Umbraco.Tests/PublishedContent/LegacyExamineBackedMediaTests.cs
@@ -5,6 +5,7 @@ using Lucene.Net.Documents;
 using Lucene.Net.Store;
 using NUnit.Framework;
 using Umbraco.Core.Configuration;
+using Umbraco.Tests.TestHelpers;
 using Umbraco.Tests.UmbracoExamine;
 using umbraco.MacroEngines;
 
@@ -15,13 +16,13 @@ namespace Umbraco.Tests.PublishedContent
         public override void TestSetup()
         {
             base.TestSetup();
-            UmbracoSettings.ForceSafeAliases = true;
-            UmbracoSettings.UmbracoLibraryCacheDuration = 1800;
-            UmbracoSettings.ForceSafeAliases = true;
+            SettingsForTests.ForceSafeAliases = true;
+            SettingsForTests.UmbracoLibraryCacheDuration = 1800;
         }
 
         public override void TestTearDown()
         {
+            SettingsForTests.Reset();
             base.TestTearDown();
         }
 

commit a2a96678205e7ecf58eb613001bbbd3317e85734

Correcting issue introduced in d3b69e04e1e5f5620e18c805d5cf0b265f140f96 where the returned IProfile would contain the Username and not the Name. This will have changed the Creator and Writer name in the xml cache to be the Login instead of the Name of the user, so for those that use these fields its important to republish to update the xml cache.
Fixes U4-2628 creatorName and writerName should use username - not loginname. Also verified through file history back to v4.

src/Umbraco.Core/Services/UserService.cs
@@ -43,13 +43,13 @@ namespace Umbraco.Core.Services
         public IProfile GetProfileById(int id)
         {
             var user = GetUserById(id);
-            return new Profile(user.Id, user.Username);
+            return new Profile(user.Id, user.Name);
         }
 
         public IProfile GetProfileByUserName(string username)
         {
             var user = GetUserByUserName(username);
-            return new Profile(user.Id, user.Username);
+            return new Profile(user.Id, user.Name);
         }
 
         public IUser GetUserByUserName(string username)