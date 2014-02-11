a0b9de59b57d762fd4fdd419b6b5fc7ae22b330d
Steve <devnull@localhost>
2014-02-11 12:30:07 +0000

    Fix for file uploads is still needed in 4.8

--- src/umbraco.cms/businesslogic/datatype/FileHandlerData.cs
+++ src/umbraco.cms/businesslogic/datatype/FileHandlerData.cs
@@ -118,13 +118,13 @@ namespace umbraco.cms.businesslogic.datatype
                         base.Value = String.Empty;
 
                         // also reset values of related fields
-                        clearRelatedValues();
+                        //clearRelatedValues();
                     }
                 }
                 else
                 {
                     base.Value = value;
-                    clearRelatedValues();
+                    //clearRelatedValues();
                 }
             }
         }
