361443586ffa15938ae21b8e14683d37d1db2afc
Steve Megson <steve@megson.org>
2019-07-27 16:50:03 +0100

    Call "nested" value converters in GridValueConverter to render macros

--- src/Umbraco.Core/PropertyEditors/ValueConverters/GridValueConverter.cs
+++ src/Umbraco.Core/PropertyEditors/ValueConverters/GridValueConverter.cs
@@ -11,6 +11,7 @@ using Umbraco.Core.Configuration.Grid;
 using Umbraco.Core.IO;
 using Umbraco.Core.Logging;
 using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.Models;
 
 namespace Umbraco.Core.PropertyEditors.ValueConverters
 {
@@ -21,11 +22,13 @@ namespace Umbraco.Core.PropertyEditors.ValueConverters
     public class GridValueConverter : JsonValueConverter
     {
         private readonly IGridConfig _config;
+        private readonly Lazy<PropertyValueConverterCollection> _propertyValueConverters;
 
-        public GridValueConverter(PropertyEditorCollection propertyEditors, IGridConfig config)
+        public GridValueConverter(PropertyEditorCollection propertyEditors, IGridConfig config, Lazy<PropertyValueConverterCollection> propertyValueConverters)
             : base(propertyEditors)
         {
             _config = config;
+            _propertyValueConverters = propertyValueConverters;
         }
 
         public override bool IsConverter(IPublishedPropertyType propertyType)
@@ -35,18 +38,18 @@ namespace Umbraco.Core.PropertyEditors.ValueConverters
             => typeof (JToken);
 
         public override PropertyCacheLevel GetPropertyCacheLevel(IPublishedPropertyType propertyType)
-            => PropertyCacheLevel.Element;
+            => PropertyCacheLevel.Snapshot;
 
-        public override object ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object source, bool preview)
+        public override object ConvertIntermediateToObject(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview)
         {
-            if (source == null) return null;
-            var sourceString = source.ToString();
+            if (inter == null) return null;
+            var interString = inter.ToString();
 
-            if (sourceString.DetectIsJson())
+            if (interString.DetectIsJson())
             {
                 try
                 {
-                    var obj = JsonConvert.DeserializeObject<JObject>(sourceString);
+                    var obj = JsonConvert.DeserializeObject<JObject>(interString);
 
                     //so we have the grid json... we need to merge in the grid's configuration values with the values
                     // we've saved in the database so that when the front end gets this value, it is up-to-date.
@@ -85,6 +88,21 @@ namespace Umbraco.Core.PropertyEditors.ValueConverters
 
                                                 control["editor"] = serialized;
                                             }
+
+                                            if (found.EditorAlias != null)
+                                            {
+                                                var propertyValueConverter = GetPropertyValueConverter(found.EditorAlias);
+                                                if ( propertyValueConverter != null)
+                                                {
+                                                    var interValue = propertyValueConverter.ConvertSourceToIntermediate(null, null, control.Value<string>("value"), preview);
+                                                    var objValue = propertyValueConverter.ConvertIntermediateToObject(null, null, PropertyCacheLevel.None, interValue, preview);
+
+                                                    if (objValue is IHtmlString)
+                                                        objValue = objValue.ToString();
+                                                    
+                                                    control["value"] = (objValue == null) ? null : JToken.FromObject(objValue);
+                                                }
+                                            }
                                         }
                                     }
                                 }
@@ -96,12 +114,12 @@ namespace Umbraco.Core.PropertyEditors.ValueConverters
                 }
                 catch (Exception ex)
                 {
-                    Current.Logger.Error<GridValueConverter>(ex, "Could not parse the string '{JsonString}' to a json object", sourceString);
+                    Current.Logger.Error<GridValueConverter>(ex, "Could not parse the string '{JsonString}' to a json object", interString);
                 }
             }
 
             //it's not json, just return the string
-            return sourceString;
+            return interString;
         }
 
         private JArray GetArray(JObject obj, string propertyName)
@@ -115,5 +133,102 @@ namespace Umbraco.Core.PropertyEditors.ValueConverters
             return new JArray();
         }
 
+        private IPropertyValueConverter GetPropertyValueConverter(string editorAlias)
+        {
+            var propertyType = new DummyPublishedPropertyType(editorAlias);
+
+            IPropertyValueConverter result = null;
+            var isdefault = false;
+
+            foreach (var converter in _propertyValueConverters.Value)
+            {
+                if (!converter.IsConverter(propertyType))
+                    continue;
+
+                if (result == null)
+                {
+                    result = converter;
+                    isdefault = _propertyValueConverters.Value.IsDefault(converter);
+                    continue;
+                }
+
+                if (isdefault)
+                {
+                    if (_propertyValueConverters.Value.IsDefault(converter))
+                    {
+                        // previous was default, and got another default
+                        if (_propertyValueConverters.Value.Shadows(result, converter))
+                        {
+                            // previous shadows, ignore
+                        }
+                        else if (_propertyValueConverters.Value.Shadows(converter, result))
+                        {
+                            // shadows previous, replace
+                            result = converter;
+                        }
+                        else
+                        {
+                            // no shadow - bad                            
+                        }
+                    }
+                    else
+                    {
+                        // previous was default, replaced by non-default
+                        result = converter;
+                        isdefault = false;
+                    }
+                }
+                else
+                {
+                    if (_propertyValueConverters.Value.IsDefault(converter))
+                    {
+                        // previous was non-default, ignore default
+                    }
+                    else
+                    {
+                        // previous was non-default, and got another non-default - bad
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private class DummyPublishedPropertyType : IPublishedPropertyType
+        {
+            private readonly string _editorAlias;
+
+            public DummyPublishedPropertyType(string editorAlias)
+            {
+                _editorAlias = editorAlias;
+            }
+
+            public string EditorAlias => _editorAlias;
+
+            public IPublishedContentType ContentType => null;
+
+            public PublishedDataType DataType => null;
+
+            public string Alias => null;
+
+
+            public bool IsUserProperty => false;
+
+            public ContentVariation Variations => ContentVariation.Nothing;
+
+            public PropertyCacheLevel CacheLevel => PropertyCacheLevel.Unknown;
+
+            public Type ModelClrType => typeof(object);
+
+            public Type ClrType => typeof(object);
+
+            public object ConvertInterToObject(IPublishedElement owner, PropertyCacheLevel referenceCacheLevel, object inter, bool preview) => inter;
+
+            public object ConvertInterToXPath(IPublishedElement owner, PropertyCacheLevel referenceCacheLevel, object inter, bool preview) => inter;
+
+            public object ConvertSourceToInter(IPublishedElement owner, object source, bool preview) => source;
+
+            public bool? IsValue(object value, PropertyValueLevel level) => null;
+        }
     }
 }
