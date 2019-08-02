007d9e38dd7ebb684b9de1224f31b78a2a892e61
Steve Megson <steve@honestjohn.co.uk>
2019-08-02 19:37:01 +0100

    ContentExistsRouteConstraint

---
+++ src/Umbraco.Web/Mvc/ContentExistsRouteConstraint.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Web;
+using System.Web.Routing;
+
+using Umbraco.Core;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web.Routing;
+
+namespace Umbraco.Web.Mvc
+{
+    public class ContentExistsRouteConstraint : IRouteConstraint
+    {
+        private static HashSet<string> _failedMatches = new HashSet<string>();
+        private static readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+
+        public static void UncacheFailure(string path)
+        {
+            using (new WriteLock(_lock))
+            {
+                _failedMatches.Remove(path);
+            }
+        }
+
+        public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            var umbracoContext = Composing.Current.UmbracoContext;
+            if (umbracoContext == null)
+            {
+                return false;
+            }
+
+            if (!umbracoContext.InPreviewMode)
+            {
+                using (new ReadLock(_lock))
+                {
+                    if (_failedMatches.Contains(umbracoContext.CleanedUmbracoUrl.GetLeftPart(UriPartial.Path)))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            var publishedRouter = Composing.Current.Factory.GetInstance<IPublishedRouter>();            
+            var request = publishedRouter.CreateRequest(umbracoContext);
+            publishedRouter.PrepareRequest(request);
+
+            if ( request.HasPublishedContent)
+            {
+                return ConstraintShouldMatchForPage(request.PublishedContent);
+            }
+            else if ( request.IsRedirect)
+            {
+                return true;
+            }
+            else
+            {
+                if (!umbracoContext.InPreviewMode)
+                {
+                    using (new WriteLock(_lock))
+                    {
+                        _failedMatches.Add(umbracoContext.CleanedUmbracoUrl.GetLeftPart(UriPartial.Path));
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        public virtual bool ConstraintShouldMatchForPage(IPublishedContent c)
+        {
+            return true;
+        }
+    }
+}
